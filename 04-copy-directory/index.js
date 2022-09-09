be048d2ef8290f1ac3b9301f034bda4261a28b27
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-09-09 15:55:20 +0300

    feat: add solution to task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+import * as fs from 'fs';
+import { mkdir, readdir, copyFile, writeFile } from "fs/promises";
 
+try {
+    fs.mkdir('files-copy', { recursive: true }, async (err) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            const files = await readdir('files', {withFileTypes: true});
+            for (let file of files) {
+                await writeFile(`./files-copy/${file.name}`, '', async (err) => {
+                    if (err) console.log(err.message);
+                });
+                await copyFile(`./files/${file.name}`, `./files-copy/${file.name}`);
+            }
+        }
+    }) 
+} catch (err) {
+    console.error(err.message);
+}
