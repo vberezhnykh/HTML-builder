3fce07252ffc1a64d5e95ea34a7d175d14a53cbe
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-11-07 16:45:52 +0300

    fix: script can be run from the main folder; remove sync func (task 6)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,76 +1,63 @@
-import { createWriteStream, read, readFile, ReadStream, write  } from "fs";
-import { readdir, writeFile, copyFile, mkdir } from "fs/promises";
+import * as fs from 'fs';
+import { copyFile, rm, writeFile } from 'fs/promises';
+import { createWriteStream, mkdir } from 'node:fs';
+import { readFile, readdir } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-try {
-    //создание папки проекта и работа с html
-    mkdir('project-dist', { recursive: true }, (err) => {
-        if (err) console.log(err.message);
-    });
-    let htmlTemplate = '';
-    readFile('template.html', 'utf8', (err, data) => {
-        if (err) throw err;
-        htmlTemplate += data;
-    })
-    
-    let articlesTemplate = '';
-    let footerTemplate = '';
-    let headerTemplate = '';
-
-    const components = await readdir('components', { withFileTypes: true })
+// создаем папку project-dist
+await rm (path.join(__dirname, 'project-dist'), { recursive: true, force: true });
+mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, (err) => {
+    if (err) throw err;
+});
+// работа с html
+const outputHtml = createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
+fs.readFile(path.join(__dirname, 'template.html'), async (err, data) => {
+    if (err) throw err;
+    let htmlTemplate = data.toString();
+    const components = await readdir(path.join(__dirname, 'components'), { withFileTypes: true })
     for (let component of components) {
-        readFile(`components/${component.name}`, 'utf8', (err, data) => {
-            if (err) throw err;
-            if (component.name.split('.')[0] === 'articles') {
-                articlesTemplate += data;
-                htmlTemplate = htmlTemplate.replace(`{{${component.name.split('.')[0]}}}`, `${articlesTemplate}`);
-            } else if (component.name.split('.')[0] === 'footer') {
-                footerTemplate += data;
-                htmlTemplate = htmlTemplate.replace(`{{${component.name.split('.')[0]}}}`, `${footerTemplate}`);
-            } else {
-                headerTemplate += data;
-                htmlTemplate = htmlTemplate.replace(`{{${component.name.split('.')[0]}}}`, `${headerTemplate}`);
-            };
-        })
-    }
-    setTimeout(async () => {
-        await writeFile('project-dist/index.html', htmlTemplate)
-    }, 1000);
-
-    //работа с css
-    const cssFiles = await readdir('styles', { withFileTypes: true });
-    let styles = [];
-    for (let file of cssFiles) {
-        const ext = file.name.split('.')[1];
-        if (file.isFile() && ext === 'css') {
-            readFile(`styles/${file.name}`, 'utf8', (err, data) => {
-                if (err) throw err;
-                styles.push(data);
-            })
+        if (component.isFile && path.extname(component.name) === '.html') {
+            const tagname = component.name.split('.')[0];
+            const content = await (await readFile(path.join(__dirname, 'components', component.name))).toString();
+            htmlTemplate = htmlTemplate.replace(`{{${tagname}}}`, content);
         }
     }
-    setTimeout(async () => {
-        await writeFile('project-dist/style.css', styles, (err) => {
-            if (err) console.log(err.message);
-        }, 1000);
-    });
-
-    //работа с assets
-    mkdir('project-dist/assets', { recursive: true });
-    const assetsFiles = await readdir('assets', { withFileTypes: true });
-    for (let asset of assetsFiles) {
+    outputHtml.write(htmlTemplate);
+})
+// работа с css
+const outputCSS = createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
+const cssFiles = await readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
+for (let cssFile of cssFiles) {
+    if (cssFile.isFile() && path.extname(cssFile.name) === '.css') {
+        fs.readFile(path.join(__dirname, 'styles', cssFile.name), (err, data) => {
+            if (err) throw err;
+            outputCSS.write(data);
+        })  
+    }
+}
+// работа с assets
+mkdir(path.join(__dirname, 'project-dist', 'assets'), { recursive: true }, (err) => {
+    if (err) throw err;
+});
+async function copyAssets(assets) {
+    for (let asset of assets) {
         if (asset.isFile()) {
-            await writeFile(`project-dist/assets/${asset.name}`, '');
-            await copyFile(`assets/${asset.name}`, `project-dist/assets/${asset.name}`);
+            await writeFile(path.join(__dirname, 'assets', asset.name), '');
+            await copyFile(path.join(__dirname, 'assets', asset.name), path.join(__dirname, 'project-dist', 'assets', asset.name));
         } else {
-            const assetsFiles = await readdir(`assets/${asset.name}`, { withFileTypes: true });
-            for (let file of assetsFiles) {
-                mkdir(`project-dist/assets/${asset.name}`, { recursive: true });
-                await writeFile(`project-dist/assets/${asset.name}/${file.name}`, '');
-                await copyFile(`assets/${asset.name}/${file.name}`, `project-dist/assets/${asset.name}/${file.name}`);
+            const filesInDir = await readdir(path.join(__dirname, 'assets', asset.name), { withFileTypes: true });
+            for (let file of filesInDir) {
+                mkdir(path.join(__dirname, 'project-dist', 'assets', asset.name), { recursive: true }, (err) => {
+                    if (err) throw err;
+                });
+                await writeFile(path.join(__dirname, 'project-dist', 'assets', asset.name, file.name), '');
+                await copyFile(path.join(__dirname, 'assets', asset.name, file.name), path.join(__dirname, 'project-dist', 'assets', asset.name, file.name));
             }
         }
     }
-    
-} catch (err) {
-    console.log(err);
 }
+const assetsFiles = await readdir(path.join(__dirname, 'assets'), { withFileTypes: true });
+copyAssets(assetsFiles);
