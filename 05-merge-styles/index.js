f1529d6583815e73fad03dcac77c1fa84819de3e
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-11-07 13:51:01 +0300

    fix: script can be run from the main folder; remove sync func

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,23 +1,22 @@
 import * as fs from 'fs';
 import { readdir, writeFile } from 'fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 try {
-    const files = await readdir('styles', { withFileTypes: true });
-    let styles = [];
+    const outputFile = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+    const files = await readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
     for (let file of files) {
         const ext = file.name.split('.')[1]
         if (file.isFile() && ext === 'css') {
-            fs.readFile(`styles/${file.name}`, 'utf8', (err, data) => {
+            fs.readFile(path.join(__dirname, `styles/${file.name}`), 'utf8', (err, data) => {
                 if (err) throw err;
-                styles.push(data);
+               outputFile.write(data);
             })
         }
     }
-    setTimeout(async () => {
-        await writeFile('project-dist/bundle.css', styles, (err) => {
-        if (err) console.log(err.message);
-    })
-    }, 100);
     
 } catch (err) {
     console.log(err.message)
