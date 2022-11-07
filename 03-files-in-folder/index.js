3f3205499d51217402c0e2237677c31ecb1911ae
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-11-07 12:11:40 +0300

    fix: script can be run from the main folder (task 3)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,12 +1,16 @@
 import { readdir, stat } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 try {
-    const files = await readdir('secret-folder', {withFileTypes: true});
+    const files = await readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});
     for (const file of files) {
         if (file.isFile()) {
             const name = file.name.split('.')[0];
             const ext = file.name.split('.')[1];
-            const fileSize = await stat(`./secret-folder/${file.name}`, (err, stats) => {
+            const fileSize = await stat(path.join(__dirname, `./secret-folder/${file.name}`), (err, stats) => {
                 if (err) console.error(err);
                 else return stats;
             });
