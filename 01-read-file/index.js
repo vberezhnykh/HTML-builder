959c0a841ded802be3691e5720fb1afcded28c24
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-11-07 11:53:14 +0300

    fix: script can be run from the main folder (task 1)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,9 @@
 import * as fs from 'node:fs';
-const stream = new fs.ReadStream("text.txt");
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const stream = new fs.ReadStream(path.join(__dirname, "text.txt"));
 stream.on('readable', () => {
     const data = stream.read();
     if (data != null) console.log(data.toString());
